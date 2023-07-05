babe89b9a36f6300a42fa2798f24c2c736f5b2f1
Yogeshswami1 <70189510+Yogeshswami1@users.noreply.github.com>
2023-07-05 12:39:36 +0530

    ipod

---
+++ build/precache-manifest.45d2af700325afe5b096738acc220632.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c139a42104d24159afc05ca302ced160",
+    "url": "/iPod/index.html"
+  },
+  {
+    "revision": "cfcd468fad2e1c5b4101",
+    "url": "/iPod/static/css/main.a2e7bd60.chunk.css"
+  },
+  {
+    "revision": "623d2a99ddffa95676e9",
+    "url": "/iPod/static/js/2.4667406a.chunk.js"
+  },
+  {
+    "revision": "d8341c57f580e145d476bfdbdb6453b4",
+    "url": "/iPod/static/js/2.4667406a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcd468fad2e1c5b4101",
+    "url": "/iPod/static/js/main.01b78fdb.chunk.js"
+  },
+  {
+    "revision": "9f2800e2662a2f09272c",
+    "url": "/iPod/static/js/runtime-main.3a22bdbc.js"
+  },
+  {
+    "revision": "d37c85cbf52a1a3b8fe11f0ea41d198b",
+    "url": "/iPod/static/media/Bas_tu.d37c85cb.mp3"
+  },
+  {
+    "revision": "b7cc3d3b1792109bded7f98388d909bb",
+    "url": "/iPod/static/media/Brown-Rang.b7cc3d3b.mp3"
+  },
+  {
+    "revision": "7b20014738dbb81eeac8872627fb673e",
+    "url": "/iPod/static/media/Heeriye.7b200147.mp3"
+  },
+  {
+    "revision": "bb2c3b1201e9dea42cdbb19bd074db4c",
+    "url": "/iPod/static/media/Heeriye.bb2c3b12.jpg"
+  },
+  {
+    "revision": "22fd016d5cf8fc0b0849f6f56e18f547",
+    "url": "/iPod/static/media/Hummer.22fd016d.mp3"
+  },
+  {
+    "revision": "005ac1ecf3afe0cab3b61b2a61e1592e",
+    "url": "/iPod/static/media/Sambh.005ac1ec.mp3"
+  },
+  {
+    "revision": "15dba15d369742bfb5cba1c6b5303ba9",
+    "url": "/iPod/static/media/bas_tu.15dba15d.jpg"
+  },
+  {
+    "revision": "c0be6a5c2bfee7f5e67ab4b810e355e6",
+    "url": "/iPod/static/media/brown.c0be6a5c.png"
+  },
+  {
+    "revision": "0f702e6d9615af4ea6e4503fa3754645",
+    "url": "/iPod/static/media/game.0f702e6d.jpg"
+  },
+  {
+    "revision": "86f652825909378dd28f81eb87ed4dd3",
+    "url": "/iPod/static/media/music.86f65282.jpg"
+  },
+  {
+    "revision": "626bd78a090cd747f7bfe1addd07babd",
+    "url": "/iPod/static/media/settings.626bd78a.png"
+  },
+  {
+    "revision": "e6885a7aeed9ad504fa0244f13ea21bd",
+    "url": "/iPod/static/media/song1-sambh.e6885a7a.jpg"
+  },
+  {
+    "revision": "76cc066f22a44e83829414e56374d292",
+    "url": "/iPod/static/media/song2-hummer.76cc066f.jpg"
+  },
+  {
+    "revision": "227f0ba9833ea10638eb851b4937d9f3",
+    "url": "/iPod/static/media/wallpaper1.227f0ba9.jpg"
+  },
+  {
+    "revision": "23077a93618a97f5d7254432926d3d69",
+    "url": "/iPod/static/media/wallpaper2.23077a93.jpg"
+  },
+  {
+    "revision": "b2fda5cab36fb65eccf326028ec5e1bb",
+    "url": "/iPod/static/media/wallpaper3.b2fda5ca.jpg"
+  }
+]);
